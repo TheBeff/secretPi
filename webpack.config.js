e45511ff9844238ce201dcee8e5d236ef2f0c265
freda nada <fredasnada@gmail.com>
2017-03-15 14:49:52 -0400

    bug fix

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     exclude: /(node_modules|bower_components)/,
     loaders: [
       {
-        test: /\.js$/, exclude: /node_modules/,
+        test: /\.jsx?$/, exclude: /node_modules/,
         loader: 'babel-loader?presets[]=es2015&presets[]=react'
       },
       {
